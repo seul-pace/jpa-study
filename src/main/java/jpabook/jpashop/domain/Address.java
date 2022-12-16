af6a4783a1578ca1936bb3f94f4e158dea2dbfd8
seulhee <lsh9412@yanadoocorp.com>
2022-12-16 13:58:50 +0900

    엔티티 클래스 개발1

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable // 어딘가에 내장이 될 수 있다
+@Getter
+public class Address {
+
+    private String city;
+
+    private String street;
+
+    private String zipcode;
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded // Embeddable 둘 중에 하나만 있어도 됨
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // orders 테이블에 있는 member에 의해 매핑된 거울일 뿐이야
+    private List<Order> orders = new ArrayList<>();
+}
